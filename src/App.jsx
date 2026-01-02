5e512ef4191ce50625e4f38bdcd2d0e59fed090e
N-1218 <malusarenamrata88@gmail.com>
2026-01-02 12:21:57 +0530

    Book Store

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Project from "./Components/Project";
+import Blog from "./Components/Blog";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Project />
+      <Blog />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
