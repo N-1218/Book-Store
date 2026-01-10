d2b1ea766d53421188b663562284f2bb5f300c24
N-1218 <malusarenamrata88@gmail.com>
2026-01-10 15:29:59 +0530

    Book Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar.jsx';
@@ -14,41 +13,21 @@ export default function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-         <Route path="/" element={<Home />} />
-        <Route path="/Blogdetails" element={<Blogdetails />} />
+      {/* <Routes>
+        <Route path="/" element={<Blogdetails />} />
+        <Route path="/Blogdetails" element={<Home />} />
         <Route path="/About" element={<About/>}/>
         <Route path="/registrationpage" element={<RegistrationPage />} />
         <Route path="/loginPage" element={<LoginPage />} />
         <Route path="/contact" element={<Contact />} />
-      </Routes>
+      </Routes> */}
+      <Home/>
+         <Blogdetails/>
+            <About/>
+               <Contact/>
 
-=======
-import "./App.css";
-import Header from "./Components/Header";
-import Hero from "./Components/Hero";
-import About from "./Components/About";
-import Project from "./Components/Project";
-import Blog from "./Components/Blog";
-import Contact from "./Components/Contact";
-import Footer from "./Components/Footer";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <About />
-      <Project />
-      <Blog />
-      <Contact />
->>>>>>> 69952499ab6155c0a3bd356a14ed86b3feb3b924
       <Footer />
     </>
   );
 }
-<<<<<<< HEAD
-=======
-
-export default App;
->>>>>>> 69952499ab6155c0a3bd356a14ed86b3feb3b924
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: "/Book-Store/"
-})
-
-
-=======
-export default defineConfig({
-  server: {
-    port: 5173,
-    strictPort: true,
-  },
 });
->>>>>>> 69952499ab6155c0a3bd356a14ed86b3feb3b924
